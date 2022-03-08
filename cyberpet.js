75e1a75bfffdb0dd8258f0808966f5588094f53b
Michael C <mikkdc@yahoo.co.uk>
2022-03-08 16:11:16 +0000

    added boredom getter and play methods

--- cyberpet.js
+++ cyberpet.js
@@ -3,6 +3,7 @@ class Animal{
         this._name = name;
         this._hunger = 10;
         this._thirst = 10;
+        this._boredom = 10;
     }
     get name() {
         return this._name;
@@ -13,10 +14,18 @@ class Animal{
     get thirst() {
         return this._thirst;
     }
+    get boredom() {
+        return this._boredom;
+    }
     eat() {
         this._hunger--;
     }
     drink() {
         this._thirst--;
     }
+    play() {
+        this._boredom--;
+    }
+    
+
 }
